d4d567f664eb4924787d50da5f9a29c4aecd0783
Sander Säde <sasde@microsoft.com>
2020-04-20 17:44:17 +0300

    Mostly done

---
+++ Main.js
@@ -0,0 +1,66 @@
+﻿function generateHtml() {
+
+    function convert(str)
+    {
+        str = str.replace(/&/g, "&amp;");
+        str = str.replace(/>/g, "&gt;");
+        str = str.replace(/</g, "&lt;");
+        str = str.replace(/"/g, "&quot;");
+        str = str.replace(/'/g, "&#039;");
+        return str;
+    }
+
+    const previewPanel = document.getElementById("previewPanel");
+    previewPanel.innerHTML = "";
+    const htmlPre = document.getElementById("htmlContent");
+    htmlPre.textContent = "";
+
+    const hasHeaders = document.querySelector('#hasHeader:checked');
+    const text = document.getElementById("input").value;
+    const split = text.split(/\r?\n/);
+
+    var output = "<table>";
+    if (hasHeaders != undefined) {
+        output += "\r\n <thead>\r\n  <tr>";
+        const tds = split[0].split(/\t/);
+
+        tds.forEach(element => {
+            output += "\r\n\   <th>" + convert(element) + "</th>";
+        });
+
+        output += "\r\n\  </tr>\r\n </thead>";
+        split.shift();
+    }
+
+
+    split.forEach(row => {
+        if (!row || row.trim().length < 1)
+            return true;
+        var tds = row.split(/\t/);
+        output += "\r\n  <tr>";
+        tds.forEach(element => {
+            output += "\r\n\   <td>" + convert(element) + "</td>";
+        });
+
+        output += "\r\n  </tr>";
+        return true;
+    });
+    output += "\r\n</table>";
+
+    previewPanel.innerHTML = output;
+
+    htmlPre.classList.remove("prettyprinted");
+    htmlPre.textContent = output;
+}
+
+
+
+function copyHtml() {
+    navigator.clipboard.writeText(document.getElementById("htmlContent").textContent).then(function() {
+        var c = document.getElementById("copy-message");
+        c.classList.remove("copy-message-animation");
+        setTimeout(() => c.classList.add("copy-message-animation"), 100);
+    }, function() {
+        alert("Failed to copy the text to clipboard!");
+    });
+}
